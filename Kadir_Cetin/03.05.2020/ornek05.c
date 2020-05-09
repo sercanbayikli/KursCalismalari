18a53bcb375a970d53349af09a5de8483af6ff2a
kadir45202 <cetinler987@gmail.com>
2020-05-09 01:01:33 +0100

    ornek5 yuklendi

---
+++ Kadir_Cetin/03.05.2020/ornek05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char isim[50];
+int i;
+
+int main(){
+
+printf("Kelimeleri aralarina bosluk koyarak giriniz\n");
+
+for(int i=0;i<=50;i++)
+{
+scanf("%s",isim);
+printf("\n%s",isim);
+}
+main();
+
+return 0;
+}
