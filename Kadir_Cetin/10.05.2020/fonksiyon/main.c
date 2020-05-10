3b7db76984e266ff0628233a7a7dcfbf5a794dda
kadir45202 <cetinler987@gmail.com>
2020-05-10 13:49:18 +0100

    fonk

---
+++ Kadir_Cetin/10.05.2020/fonksiyon/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <fonk1.h>
+#include <fonk2.h>
+#include <fonk3.h>
+
+char kelime[100]={"Terminal_daha_zevkli"};
+
+int main() {
+
+  fonk1();
+  fonk2();
+  fonk3();
+
+  return 0;
+}
