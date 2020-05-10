5f5bd58c5e95f2b2bfad0e583d36f062a0389aac
Your Name <you@example.com>
2020-05-10 13:47:31 +0100

    Terminal Daha Zevkli

---
+++ Efe_Turbedar/10.05.2020/KursGunu/Calısma_3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "fonk1.h"
+#include "fonk2.h"
+#include "fonk3.h"
+
+int main(){
+ fonk1("Terminal_Daha_Zevkli");
+ fonk2("Terminal_Daha_Zevkli");
+ fonk3("Terminal_Daha_Zevkli");
+ return 0;
+}
