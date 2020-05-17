3ad3300c57b3eaf6438fe79ca9c59a0d23be5d0d
Your Name <you@example.com>
2020-05-17 14:47:50 +0300

    bitti

---
+++ Cansu_Oran/17.05.2020/day02/ex00.c
@@ -0,0 +1,22 @@
+void ft_putchar(char c);
+ void ft_print_alphabet();
+
+int main(){
+ ft_print_alphabet();
+}
+
+
+
+
+
+
+ void ft_print_alphabet(){
+int a='a';
+while(a<='z'){
+ft_putchar(a);
+a++;
+}
+
+
+
+}
---
+++ Cansu_Oran/17.05.2020/day02/ex06.c
@@ -0,0 +1,28 @@
+void ft_putchar(char c);
+void ft_putnbr(int x);
+
+int main(){
+
+ft_putnbr(-42);
+
+}
+
+
+void ft_putnbr(int x){
+if(x>=0 && x< 10){
+ ft_putchar((x+'0'));
+
+}
+else if(x>=10){
+ft_putnbr(x/10);
+ft_putchar(((x%10)+'0'));
+
+}
+else{
+ft_putchar('-');
+ft_putnbr(x*-1);
+
+
+}
+
+}
