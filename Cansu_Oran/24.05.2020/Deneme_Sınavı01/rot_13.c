0c4742d431cd5fa2808bc4455ec40a5712300ba6
Your Name <you@example.com>
2020-05-24 16:39:18 +0300

    bitti

--- Cansu_Oran/24.05.2020/Deneme_Sınavı01/rot_13.c
+++ Cansu_Oran/24.05.2020/Deneme_Sınavı01/rot_13.c
@@ -4,13 +4,21 @@ int main(int argc,char *argv[]){
 int a=0;
 
 while(argv[1][a]){
-if(argv[1][a]<91 && argv[1][a]>64){
- argv[1][a]=argv[1][a]+13;
-if(argv[1][a]>=91)
-}
-else if(argv[1][a]<123 && argv[1][a]>96)  argv[1][a]=argv[1][a]-32;
 
-
+if (argv[1][a] >= 'A' && argv[1][a] <= 'M'){
+ft_putchar(argv[1][a] + 13);
+}
+else if (argv[1][a] >= 'N' && argv[1][a] <= 'Z'){
+ft_putchar(argv[1][a] - 13);}
+else if (argv[1][a] >= 'a' && argv[1][a] <= 'm'){
+ft_putchar(argv[1][a] + 13);
+}
+else if (argv[1][a] >= 'n' && argv[1][a] <= 'z'){
+ft_putchar(argv[1][a] - 13);
+}
+else{
+ft_putchar(argv[1][a]);
 }
 
-}
+a++;
+}}
