0d83e05a4b9b18c72db7bc567f9d3b99de4434da
Your Name <you@example.com>
2020-05-24 15:55:46 +0300

    yarım

---
+++ Cansu_Oran/24.05.2020/Deneme_Sınavı01/rot_13.c
@@ -0,0 +1,16 @@
+void ft_putchar(char c);
+
+int main(int argc,char *argv[]){
+int a=0;
+
+while(argv[1][a]){
+if(argv[1][a]<91 && argv[1][a]>64){
+ argv[1][a]=argv[1][a]+13;
+if(argv[1][a]>=91)
+}
+else if(argv[1][a]<123 && argv[1][a]>96)  argv[1][a]=argv[1][a]-32;
+
+
+}
+
+}
