ae354a2fc1b01768499e490a30caf9784549ebe5
Your Name <you@example.com>
2020-05-27 16:58:36 +0300

    sastantuayı güncelledim

--- Cansu_Oran/Sastantua/sastantua.c
+++ Cansu_Oran/Sastantua/sastantua.c
@@ -1,12 +1,12 @@
 void sastantua(int x);
 void ft_putchar(char c);
 void draw(int x,int toplam);
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char *argv[]){
     if(argc!=2){
         printf("Eksik veya fazla sayı girdiniz.");
-        exit(1);
+        return 0;
     }
     
     sastantua(atoi(argv[1]));
@@ -14,53 +14,37 @@ int main(int argc, char *argv[]){
 }
 
 void sastantua(int x){
-    int y=3,toplam=1,dev=1,cık=4;
-    for(int a=1;a<=x;a++){
-        for(int b=1;b<=y;b++){
-            toplam=toplam+2;
-            
-        }
-        y++;
-        if(dev+2==a){
-            cık=cık+2;
-            dev=a;
-        }
-        toplam=toplam+cık;}
-    draw(x,toplam-cık);
+    int toplam;
+    if(x%2==0) toplam=((x+2)*x);
+    else toplam=((x+2)*x-1);
+    draw(x,toplam+5);
     
 }
-
 void draw(int x,int toplam){
-    int bos=((toplam-1)/2-1),b=1,l=2,cık=2,dev=1;
-    int h=toplam-2*((2*toplam-6)/4-1),j=-2;
+    int bos=toplam,b=1,cık=2,dev=1,c, door = 1 + 2 * ((x - 1) / 2);
     
     for(int a=1;a<=x;a++){// birim
-        for(int p=1;p<=l+a;p++){// satır
+        for(int p=1;p<=2+a;p++){// satır
             for(int bo=1;bo<=bos;bo++){// boşluk
                 ft_putchar(' ');
             }
+            c=5+b;
             ft_putchar('/');
             for(int k=1;k<=b;k++){
-                ft_putchar('*');
-                if(a==x && p>=x/2+1 && k==((2*toplam-6)/4-2)-x/2+j){
-                    j++;
-                    for(int r=1;r<=h;r++){
-                        
-                        if(p==x/2+2&& r== h-1){
-                            ft_putchar('$');
-                            
-                        }
-                        else{
-                            ft_putchar('|');
-                        }
-                        
-                        
-                        
-                    }
+                if(a==x &&  k>=(c-door)/2-1 && k<(c+door)/2-1 && 3+a-p<=door){
                     
-                    k=k+h;
+                    
+                    if (door >= 5 && p == 3 + a - door / 2-1  && k == (c + door) / 2 - 3)
+                        ft_putchar('$');
+                    else ft_putchar('|');
+                    
                 }
+                else  ft_putchar('*');
+                
+                
+                
             }
+            
             ft_putchar('\\');
             ft_putchar('\n');
             bos--;
