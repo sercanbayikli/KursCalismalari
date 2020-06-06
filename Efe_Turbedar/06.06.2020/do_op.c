c37b3be525985505a6599fdc86685832b0b89c8b
Your Name <you@example.com>
2020-06-06 18:54:50 +0100

    az bi düzeltme

--- Efe_Turbedar/06.06.2020/do_op.c
+++ Efe_Turbedar/06.06.2020/do_op.c
@@ -1,22 +1,7 @@
 int ii,jj[100],zz=0;
 #include <stdio.h>
 int math(char *str1,char *str2,char *str3);
-
-int ft_atoi(char *c){
-int n=0,a=0;
-if (c[0] == '-' && c[1] >= '0' && c[1] <= '9'){
-n = ft_atoi(++c);
-return (n * -1);
-}
-else if (c[0] != '-'){
- while (c[a] >= '0' && c[a] <= '9'){
-  n=(n * 10)+(c[a] - '0');
-a++;
-}}
-return (n);
-
-}
-
+#include "ft_putchar.c"
 
 int main(){
 
--- Efe_Turbedar/06.06.2020/ft_atoi.c
+++ Efe_Turbedar/06.06.2020/ft_atoi.c
@@ -1,6 +1,6 @@
-int i,j[100],z;
+int i,j,z;
 int ft_atoi(char *str){
-        for(i=0;str[i]!='\0';i++){j[i]=(str[i]-48);z=z+j[i];z=z*10;}
+        for(i=0;str[i]!='\0';i++){j=(str[i]-48);z=z+j;z=z*10;}
 		z=z/10;
 		return z;
 }
