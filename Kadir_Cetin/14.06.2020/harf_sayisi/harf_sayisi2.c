8fc70c3b07e9c5379de6fb1c5287a83c4b75dbfc
kadir45202 <cetinler987@gmail.com>
2020-06-20 10:35:34 +0100

    harf sayisi

---
+++ Kadir_Cetin/14.06.2020/harf_sayisi/harf_sayisi2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int a;
+int sayi[26] = {0};
+
+int main() {
+
+FILE *file;	
+FILE *file1;	
+
+file = fopen("Kelimeler_TR_Zemberek.txt","r");
+while((a = getc(file))!= EOF) {
+	if(a >= 97 && a <= 122) {
+	  sayi[a - 'a']++;
+}
+}
+file1 = fopen("harf_sayisi.txt","w");
+for(int i = 0; i < 26; i++) {
+fprintf(file,"%c = %d\n",('a' + i) - 32,sayi[i]);
+}
+fclose(file);
+fclose(file1);
+return 0;
+}
